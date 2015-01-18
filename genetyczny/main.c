55be1bcfabdc50edec6856772ed573d7cb9f5665
Lukasz Dworakowski <dworakowski.lukasz@gmail.com>
2015-01-18 16:38:47 +0100

    Initial Commit

---
+++ genetyczny/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  genetyczny
+//
+//  Created by Lukasz Dworakowski on 18/01/15.
+//  Copyright (c) 2015 Dworakowski. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
